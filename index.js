54a8616dd2926dc16be6be2b0091723b90e17bc1
pranjal <pranjalmisra2410@gmail.com>
2020-05-21 05:24:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+const prog = require('caporal');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
+
+prog.version('0.0.1').argument('[filename]', 'Name of the file').action(async ({ filename }) => {
+	const name = filename || 'index.js';
+	try {
+		await fs.promises.access(name);
+	} catch (err) {
+		throw new Error(` Can't find the specified filename ${name}`);
+	}
+	let proc;
+	const start = debounce(() => {
+		if (proc) {
+			console.log(chalk.redBright('>>>>>Killing an existing process<<<<<'));
+			proc.kill();
+		}
+		console.log(chalk.greenBright('>>>>>Starting a new process<<<<<'));
+		proc = spawn('node', [ name ], { stdio: 'inherit' });
+	}, 120);
+	chokidar
+		.watch('.', {
+			ignored: /\.git.|node_modules/
+		})
+		.on('add', start)
+		.on('change', start)
+		.on('unlink', start);
+});
+prog.parse(process.argv);
+// const debounce = require('lodash.debounce');
+// const chokidar = require('chokidar');
+// const prog = require('caporal');
+// const chalk = require('chalk');
+// const fs = require('fs');
+// const { spawn } = require('child_process');
+
+// prog.version('0.0.1').argument('[filename]', 'Name of a file to execute').action(async ({ filename }) => {
+// 	const name = filename || 'index.js';
+
+// 	try {
+// 		await fs.promises.access(name);
+// 	} catch (err) {
+// 		throw new Error(`Could not find the file: ${name}`);
+// 	}
+// 	let pid;
+// 	const start = debounce(() => {
+// 		if (pid) {
+// 			console.log(chalk.red('------- Ending Process -------'));
+// 			pid.kill();
+// 		}
+// 		console.log(chalk.green('------- Starting Process -------'));
+// 		pid = spawn('node', [ name ], { stdio: 'inherit' });
+// 	}, 100);
+
+// 	chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
+// });
+
+// prog.parse(process.argv);
